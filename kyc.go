cf44617d6c0213d904611ccf9656778ea3445abb
iqquee <hisyntax0@gmail.com>
2023-08-13 23:41:32 +0100

    some more config set and Nigeria BVN implemented

--- kyc.go
+++ kyc.go
@@ -1,1 +1,87 @@
 package youverify
+
+type (
+	// BankVerificationNumberRequest is the request object for BankVerificationNumber() to verify a BVN
+	BankVerificationNumberRequest struct {
+		BVN              string                 `json:"id"`
+		MetaData         map[string]interface{} `json:"metadata"`
+		IsSubjectConsent bool                   `json:"isSubjectConsent"` // This field must be true
+		PremiumBVN       bool                   `json:"premiumBVN"`
+	}
+
+	// BankVerificationNumberResponse is the response object for the BankVerificationNumber()
+	BankVerificationNumberResponse struct {
+		Success    bool   `json:"success"`
+		StatusCode int    `json:"statusCode"`
+		Message    string `json:"message"`
+		Data       struct {
+			Id                    string      `json:"id"`
+			ParentId              interface{} `json:"parentId"`
+			Status                string      `json:"status"`
+			Reason                interface{} `json:"reason"`
+			DataValidation        bool        `json:"dataValidation"`
+			SelfieValidation      bool        `json:"selfieValidation"`
+			FirstName             string      `json:"firstName"`
+			MiddleName            interface{} `json:"middleName"`
+			LastName              string      `json:"lastName"`
+			Image                 string      `json:"image"`
+			EnrollmentBranch      interface{} `json:"enrollmentBranch"`
+			EnrollmentInstitution interface{} `json:"enrollmentInstitution"`
+			Mobile                string      `json:"mobile"`
+			DateOfBirth           string      `json:"dateOfBirth"`
+			IsConsent             bool        `json:"isConsent"`
+			IdNumber              string      `json:"idNumber"`
+			Nin                   string      `json:"nin"`
+			ShouldRetrivedNin     bool        `json:"shouldRetrivedNin"`
+			BusinessId            string      `json:"businessId"`
+			Type                  string      `json:"type"`
+			AllValidationPassed   bool        `json:"allValidationPassed"`
+			RequestedAt           string      `json:"requestedAt"`
+			RequestedById         string      `json:"requestedById"`
+			Country               string      `json:"country"`
+			CreatedAt             string      `json:"createdAt"`
+			LastModifiedAt        string      `json:"lastModifiedAt"`
+			Email                 interface{} `json:"email"`
+			RegistrationDate      string      `json:"registrationDate"`
+			Gender                string      `json:"gender"`
+			LevelOfAccount        interface{} `json:"levelOfAccount"`
+			Address               struct {
+				Town        interface{} `json:"town"`
+				Lga         interface{} `json:"lga"`
+				State       interface{} `json:"state"`
+				AddressLine interface{} `json:"addressLine"`
+			} `json:"address"`
+			Title         interface{} `json:"title"`
+			MaritalStatus interface{} `json:"maritalStatus"`
+			LgaOfOrigin   interface{} `json:"lgaOfOrigin"`
+			OtherMobile   interface{} `json:"otherMobile"`
+			StateOfOrigin interface{} `json:"stateOfOrigin"`
+			WatchListed   interface{} `json:"watchListed"`
+			NameOnCard    interface{} `json:"nameOnCard"`
+			FullDetails   bool        `json:"fullDetails"`
+			Metadata      interface{} `json:"metadata"`
+			RequestedBy   struct {
+				FirstName  string `json:"firstName"`
+				LastName   string `json:"lastName"`
+				MiddleName string `json:"middleName"`
+				Id         string `json:"id"`
+			} `json:"requestedBy"`
+		} `json:"data"`
+		Links []interface{} `json:"links"`
+	}
+)
+
+// BankVerificationNumber is method used to verify BVN(Bank Verification Number) in Nigeria
+func (n *Nigeria) BankVerificationNumber(request BankVerificationNumberRequest) (*BankVerificationNumberResponse, error) {
+	if !request.IsSubjectConsent {
+		return nil, errSubjectConsent
+	}
+
+	url := "identity/ng/bvn"
+	var response BankVerificationNumberResponse
+	if err := newRequest(methodPOST, url, request, response); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
--- youverify.go
+++ youverify.go
@@ -3,7 +3,6 @@ package youverify
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 
@@ -12,42 +11,58 @@ import (
 
 const (
 	// SandboxBaseURL is the test environment base url
-	SandboxBaseURL string = "https://api.sandbox.youverify.co/"
+	SandboxBaseURL string = "https://api.sandbox.youverify.co/v2/api/"
 	// ProductionBaseURL is the live environment base url
-	ProductionBaseURL string = "https://api.youverify.co/"
+	ProductionBaseURL string = "https://api.youverify.co/v2/api/"
+	// methodPOST for an http POST method
+	methodPOST string = "POST"
+	// methodGET for an http GET method
+	methodGET string = "GET"
+)
 
-	// StatusCode200 when the server returns a status code 200
-	StatusCode200 StatusCode = "[Successful] - Your request was successful."
-	// StatusCode400 when the server returns a status code 400
-	StatusCode400 StatusCode = "[Bad request] - Most likely an invalid syntax. Check all parameters."
-	// StatusCode401 when the server returns a status code 401
-	StatusCode401 StatusCode = "[Unauthorized] - You are unauthorized for this request. Contact support@youverify.co."
-	// StatusCode402 when the server returns a status code 402
-	StatusCode402 StatusCode = "[Payment Required] - You balance is most likely low and you are required to top up your balance."
-	// StatusCode404 when the server returns a status code 404
-	StatusCode404 StatusCode = "[Not found] - URL not recognized. Check to confirm the right URL."
-	// StatusCode405 when the server returns a status code 405
-	StatusCode405 StatusCode = "[Method not found] - The request is disabled. Check the URL or rollback recent upgrades."
-	// StatusCode408 when the server returns a status code 408
-	StatusCode408 StatusCode = "[Request timeout] - Your request took longer than it should have. Check your internet connection."
-	// StatusCode424 when the server returns a status code 424
-	StatusCode424 StatusCode = "[Failed Dependency] - Third Party service Failure."
-	// StatusCode429 when the server returns a status code 429
-	StatusCode429 StatusCode = "[Too many requests] - You have sent too many requests that has exceeded the rate limit. You need to wait a while."
-	// StatusCode500 when the server returns a status code 500
-	StatusCode500 StatusCode = "[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co"
-	// StatusCode502 when the server returns a status code 502
-	StatusCode502 StatusCode = "[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co"
-	// StatusCode503 when the server returns a status code 503
-	StatusCode503 StatusCode = "[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co"
-	// StatusCode504 when the server returns a status code 504
-	StatusCode504 StatusCode = "[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co"
+var (
+	// baseURL holds the value of the baseURL
+	baseURL string
+	// secretToken holds the value of the secretToken
+	secretToken string
+	// client holds the http client
+	client *http.Client
+
+	// ErrStatusCode200 when the server returns a status code 200
+	ErrStatusCode200 StatusCode = errors.New("[Successful] - Your request was successful.")
+	// ErrStatusCode400 when the server returns a status code 400
+	ErrStatusCode400 StatusCode = errors.New("[Bad request] - Most likely an invalid syntax. Check all parameters.")
+	// ErrStatusCode401 when the server returns a status code 401
+	ErrStatusCode401 StatusCode = errors.New("[Unauthorized] - You are unauthorized for this request. Contact support@youverify.co.")
+	// ErrStatusCode402 when the server returns a status code 402
+	ErrStatusCode402 StatusCode = errors.New("[Payment Required] - You balance is most likely low and you are required to top up your balance.")
+	// ErrStatusCode404 when the server returns a status code 404
+	ErrStatusCode404 StatusCode = errors.New("[Not found] - URL not recognized. Check to confirm the right URL.")
+	// ErrStatusCode405 when the server returns a status code 405
+	ErrStatusCode405 StatusCode = errors.New("[Method not found] - The request is disabled. Check the URL or rollback recent upgrades.")
+	// ErrStatusCode408 when the server returns a status code 408
+	ErrStatusCode408 StatusCode = errors.New("[Request timeout] - Your request took longer than it should have. Check your internet connection.")
+	// ErrStatusCode424 when the server returns a status code 424
+	ErrStatusCode424 StatusCode = errors.New("[Failed Dependency] - Third Party service Failure.")
+	// ErrStatusCode429 when the server returns a status code 429
+	ErrStatusCode429 StatusCode = errors.New("[Too many requests] - You have sent too many requests that has exceeded the rate limit. You need to wait a while.")
+	// ErrStatusCode500 when the server returns a status code 500
+	ErrStatusCode500 StatusCode = errors.New("[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co")
+	// ErrStatusCode502 when the server returns a status code 502
+	ErrStatusCode502 StatusCode = errors.New("[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co")
+	// ErrStatusCode503 when the server returns a status code 503
+	ErrStatusCode503 StatusCode = errors.New("[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co")
+	// ErrStatusCode504 when the server returns a status code 504
+	ErrStatusCode504  StatusCode = errors.New("[Server error] - This is a very rare occurrence where the server is unable to process a request properly. Contact support@youverify.co")
+	errSubjectConsent            = errors.New("Subject consent field must be true")
 )
 
 type (
 	// StatusCode type
-	StatusCode string
+	StatusCode error
 
+	// Client default client for youverify
+	Client struct{}
 	// Kyc for the KYC (Know Your Customer)
 	Kyc struct{}
 	// Kyb for the KYB (Know Your Business)
@@ -57,79 +72,118 @@ type (
 	// AML for the AML (Anti-Money Laundry)
 	AML struct{}
 
-	youverify struct {
-		Http        *http.Client
-		BaseURL     string
-		SecretToken string
-	}
-
-	InitializePayload struct {
+	// InitializeRequest is the payload that initializes youverify
+	InitializeRequest struct {
 		IsLive      bool
 		SecretToken string
 		Http        *http.Client
 	}
 
-	Client struct {
-		Kyc             Kyc
-		Kyb             Kyb
-		youverify       youverify
-		RiskInteligence RiskInteligence
-		AMl             AML
-	}
+	Nigeria struct{}
 )
 
-func Initialize(i InitializePayload) *Client {
-	var baseUrl string
-	if i.IsLive {
-		baseUrl = ProductionBaseURL
+/*
+Initialize is a function that sets up youverify configs.
+
+This function takes in three(3) paramaters: Http - which is the http.Client{} for making requests, IsLive - is a bool which specifies the environment and SecretToken - is the secretToken used to authorize requests.
+*/
+func Initialize(Http *http.Client, IsLive bool, SecretToken string) *Client {
+	// var baseUrl string
+	if IsLive {
+		baseURL = ProductionBaseURL
 	} else {
-		baseUrl = SandboxBaseURL
+		baseURL = SandboxBaseURL
 	}
 
-	return &Client{
-		youverify: youverify{
-			BaseURL:     baseUrl,
-			SecretToken: i.SecretToken,
-			Http:        i.Http,
-		},
-	}
+	secretToken = SecretToken
+	client = Http
+	return &Client{}
+}
+
+// KYC returns methods for KYC (Know Your Customer)
+func (c *Client) KYC() *Kyc {
+	return &Kyc{}
+}
+
+// Nigeria returns methods for KYC (Know Your Customer) in Nigeria
+func (c *Kyc) Nigeria() *Nigeria {
+	return &Nigeria{}
+}
+
+// KYC returns methods for KYB (Know Your Business)
+func (c *Client) KYB() *Kyb {
+	return &Kyb{}
 }
 
 /*
-newRequest makes a http request to the urlbox server and decodes the server response into the reqBody parameter passed into the newRequest method
+newRequest makes a http request to youverify server and decodes the server response into the resp(esponse) parameter passed into the newRequest method
 */
-func (c *Client) newRequest(method, reqURL string, reqBody interface{}) ([]byte, int, error) {
-	newURL := c.youverify.BaseURL + reqURL
+func newRequest(method, reqURL string, reqBody, resp interface{}) error {
+	newURL := baseURL + reqURL
 	var body io.Reader
 
 	if reqBody != nil {
 		bb, err := json.Marshal(reqBody)
 		if err != nil {
-			return nil, 0, errors.Wrap(err, "http client ::: unable to marshal request struct")
+			return errors.Wrap(err, "http client ::: unable to marshal request struct")
 		}
+
 		body = bytes.NewReader(bb)
 	}
 
 	req, err := http.NewRequest(method, newURL, body)
-	bearer := fmt.Sprintf("Bearer %v", c.youverify.BaseURL)
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", bearer)
+	req.Header.Set("Token", secretToken)
 
 	if err != nil {
-		return nil, 0, errors.Wrap(err, "http client ::: unable to create request body")
+		return errors.Wrap(err, "http client ::: unable to create request body")
 	}
 
-	res, err := c.youverify.Http.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
-		return nil, 0, errors.Wrap(err, "http client ::: client failed to execute request")
+		return errors.Wrap(err, "http client ::: client failed to execute request")
 	}
 
 	defer res.Body.Close()
 
-	b, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, 0, errors.Wrap(err, "http client ::: client failed to read file")
+	if err := parseStatusCode(res.StatusCode); err != nil {
+		return err
 	}
 
-	return b, res.StatusCode, nil
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		return errors.Wrap(err, "http client ::: unable to unmarshal response body")
+	}
+
+	return nil
+}
+
+// ParseStatusCode checks the status code response from the server and returns an error for that status code accordingly
+func parseStatusCode(code int) error {
+	if code == 400 {
+		return StatusCode(ErrStatusCode400)
+	} else if code == 401 {
+		return StatusCode(ErrStatusCode401)
+	} else if code == 402 {
+		return StatusCode(ErrStatusCode402)
+	} else if code == 404 {
+		return StatusCode(ErrStatusCode404)
+	} else if code == 405 {
+		return StatusCode(ErrStatusCode405)
+	} else if code == 408 {
+		return StatusCode(ErrStatusCode408)
+	} else if code == 424 {
+		return StatusCode(ErrStatusCode424)
+	} else if code == 429 {
+		return StatusCode(ErrStatusCode429)
+	} else if code == 500 {
+		return StatusCode(ErrStatusCode500)
+	} else if code == 502 {
+		return StatusCode(ErrStatusCode502)
+	} else if code == 503 {
+		return StatusCode(ErrStatusCode503)
+	} else if code == 504 {
+		return StatusCode(ErrStatusCode504)
+	}
+
+	return nil
 }
